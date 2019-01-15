a055400a04a7db11006619cb18b11463252ef8e0
Arturo Filastò <arturo@filasto.net>
2019-01-16 09:41:39 +1300

    Add a very basic unittest

---
+++ measurement_kit_test.go
@@ -0,0 +1,20 @@
+package mk
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestFacebookMessenger(t *testing.T) {
+	nt = NewNettest("FacebookMessenger")
+	nt.Options = NettestOptions{
+		DisableCollector: true,
+		GeoIPASNPath: "GeoLite2-ASN.mmdb",
+		GeoIPCountryPath: "GeoLite2-Country.mmdb",
+		OutputPath: "facebook-result.jsonl",
+	}
+	nt.On("*", func(e mk.Event) {
+		fmt.Println(e)
+	})
+	nt.Run()
+}
