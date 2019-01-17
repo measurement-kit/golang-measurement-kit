1a2ef3888c876854bdc995b214885a433c1962ab
Arturo Filastò <arturo@filasto.net>
2019-01-17 15:08:09 +1300

    Take lib order from: https://github.com/measurement-kit/android-libs/blob/master/jni/Android.mk.in#L53

--- measurement_kit.go
+++ measurement_kit.go
@@ -31,12 +31,12 @@ package mk
 // #cgo linux,amd64 CFLAGS: -I${SRCDIR}/libs/MK_DIST/linux/measurement-kit/0.9.1+1/x86_64/include
 // #cgo linux,amd64 LDFLAGS: ${SRCDIR}/libs/MK_DIST/linux/measurement-kit/0.9.1+1/x86_64/lib/libmeasurement_kit.a
 // #cgo linux,amd64 LDFLAGS: ${SRCDIR}/libs/MK_DIST/linux/libmaxminddb/1.3.2+3/x86_64/lib/libmaxminddb.a
+// #cgo linux,amd64 LDFLAGS: ${SRCDIR}/libs/MK_DIST/linux/curl/7.63.0+2/x86_64/lib/libcurl.a
+// #cgo linux,amd64 LDFLAGS: ${SRCDIR}/libs/MK_DIST/linux/libevent/2.1.8+4/x86_64/lib/libevent_openssl.a
 // #cgo linux,amd64 LDFLAGS: ${SRCDIR}/libs/MK_DIST/linux/libressl/2.8.3+1/x86_64/lib/libssl.a
 // #cgo linux,amd64 LDFLAGS: ${SRCDIR}/libs/MK_DIST/linux/libressl/2.8.3+1/x86_64/lib/libcrypto.a
-// #cgo linux,amd64 LDFLAGS: ${SRCDIR}/libs/MK_DIST/linux/curl/7.63.0+2/x86_64/lib/libcurl.a
 // #cgo linux,amd64 LDFLAGS: ${SRCDIR}/libs/MK_DIST/linux/libevent/2.1.8+4/x86_64/lib/libevent_core.a
 // #cgo linux,amd64 LDFLAGS: ${SRCDIR}/libs/MK_DIST/linux/libevent/2.1.8+4/x86_64/lib/libevent_extra.a
-// #cgo linux,amd64 LDFLAGS: ${SRCDIR}/libs/MK_DIST/linux/libevent/2.1.8+4/x86_64/lib/libevent_openssl.a
 // #cgo linux,amd64 LDFLAGS: ${SRCDIR}/libs/MK_DIST/linux/libevent/2.1.8+4/x86_64/lib/libevent_pthreads.a
 import "C"
 
