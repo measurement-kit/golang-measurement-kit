8cb0baedf1b14a400140d8411e16cb64db98560b
Simone Basso <bassosimone@gmail.com>
2019-05-10 00:09:24 +0200

    clarify

--- internal/systemx/systemx_windows.go
+++ internal/systemx/systemx_windows.go
@@ -7,7 +7,7 @@ import (
 
 // CABundlePath returns the CA bundle path to use. If the provided path
 // is not empty, that is used. Otherwise, we'll see whether this path is
-// provided as part of a well know environment variable.
+// provided as part of the MEASUREMENT_KIT_CA_BUNDLE_PATH env variable.
 func CABundlePath(path string) string {
 	if path == "" {
 		return os.Getenv("MEASUREMENT_KIT_CA_BUNDLE_PATH")
